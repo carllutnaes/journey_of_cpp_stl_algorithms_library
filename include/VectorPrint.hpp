0d6ac0bfbb35b1f50324c8f49f315c23b3ee8ef4
carllutnaes <carl.lutnaes@gmail.com>
2021-02-24 17:10:57 +0100

    comment cleanup

--- include/VectorPrint.hpp
+++ include/VectorPrint.hpp
@@ -3,8 +3,6 @@
 #define VECTORPRINT_H
 
 std::ostream &operator<<(std::ostream &s, std::vector<int> &vec);
-//std::ostream heapTreeIllustration(const std::vector<int> &vec);
-//std::ostream heapTreeIllustration(std::ostream &s, const std::vector<int> &vec);
 
 #endif //VECTORPRINT_H
 
